650491e8606f137370e44c0508eadd7faba2de95
BW <ddc.16.12.95@gmail.com>
2020-06-06 20:52:26 +0700

    ini cuk

--- login/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host       = "localhost";
-    $user       = "root";
-    $password   = "";
-    $database   = "suratperintahlembur";
-    $connect    = mysqli_connect($host, $user, $password, $database)
-?>
--- login/welcome.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-?>
-
-Selamat Datang Users <?php echo $_SESSION['nama_lengkap']?>
-
-<a href="logout.php">Logout</a>
