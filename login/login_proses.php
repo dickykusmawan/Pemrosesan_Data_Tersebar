d6238a1f8a039a2f1422407a714d69d0504d5648
BW <ddc.16.12.95@gmail.com>
2020-06-06 19:53:59 +0700

    coba cek cuk

---
+++ login/login_proses.php
@@ -0,0 +1,19 @@
+<?php
+$username   = $_POST['username'];
+$pass       = md5($_POST['password']);
+
+include 'koneksi.php';
+
+$user = mysqli_query($connect,"select * from users where username='$username' and password='$pass'");
+$chek = mysqli_num_rows($user);
+if($chek>0)
+{
+    session_start();
+    $row = mysqli_fetch_array($user);
+    $_SESSION['nama'] = $row['nama'];
+    header("location:welcome.php");
+}else
+{
+    header("location:login.php");
+}
+?>
